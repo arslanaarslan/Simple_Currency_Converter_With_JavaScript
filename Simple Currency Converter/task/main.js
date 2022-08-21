40bbb35e9ff412602397edaa9e2893ef5e9c202a
Arslan Arslan <arslanaarslan@gmail.com>
2022-08-21 15:21:14 +0300

    Initial Commit

---
+++ Simple Currency Converter/task/main.js
@@ -0,0 +1,67 @@
+input = require('sync-input');
+
+let currencies = {
+  'JPY': 113.5,
+  'EUR': 0.89,
+  'RUB': 74.36,
+  'USD': 1,
+  'GBP': 0.75
+};
+
+console.log("Welcome to Currency Converter!");
+console.log("1 USD equals 1 USD");
+console.log("1 USD equals 113.5 JPY");
+console.log("1 USD equals 0.89 EUR");
+console.log("1 USD equals 74.36 RUB");
+console.log("1 USD equals 0.75 GBP");
+
+while (true) {
+  console.log("What do you want to do?");
+  console.log("1-Convert currencies 2-Exit program");
+  let processSelection = Number(input());
+
+  if ([1, 2].includes(processSelection)) {
+    if (processSelection === 2) {
+      break;
+    }
+  } else {
+    console.log("Unknown input");
+    continue;
+  }
+
+  while (true) {
+    console.log("What do you want to convert?");
+    let fromCurrency = input("From: ").toUpperCase();
+
+    if (!Object.keys(currencies).includes(fromCurrency)) {
+      console.log("Unknown currency");
+      continue;
+    }
+
+    let toCurrency = input("To: ").toUpperCase();
+
+    if (!Object.keys(currencies).includes(toCurrency)) {
+      console.log("Unknown currency");
+      continue;
+    }
+
+    let amount = Number(input("Amount: "));
+    let result;
+
+    if (isNaN(amount)) {
+      console.log("The amount has to be a number");
+      continue;
+    } else if (amount < 1) {
+      console.log("The amount cannot be less than 1");
+      continue;
+    } else {
+      result = amount / currencies[fromCurrency] * currencies[toCurrency];
+    }
+
+    console.log(`Result: ${amount} ${fromCurrency} equals ${result.toFixed(4)} ${toCurrency}`);
+    break;
+  }
+
+}
+
+console.log("Have a nice day!");
